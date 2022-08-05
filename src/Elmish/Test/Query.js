9093495977c56f1c7180653e80ccefb8c791edb6
Fyodor Soikin <name.fa@gmail.com>
2022-08-05 13:40:22 -0400

    Safer text

--- src/Elmish/Test/Query.js
+++ src/Elmish/Test/Query.js
@@ -1,3 +1,3 @@
-export const innerText_ = e => e.innerText
-export const outerHTML_ = e => e.outerHTML
+export const innerText_ = e => e.innerText || ""
+export const outerHTML_ = e => e.outerHTML || ""
 export const prop_ = (name, e) => e[name]
