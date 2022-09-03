ddd52dce81d12345be85a657b13b2832a7d28e90
Fyodor Soikin <name.fa@gmail.com>
2022-09-02 20:23:00 -0400

    checked and href DOM props

--- src/Elmish/Test/DomProps.purs
+++ src/Elmish/Test/DomProps.purs
@@ -4,4 +4,8 @@ newtype DomProp (a :: Type) = DomProp String
 
 value = DomProp "value" :: DomProp String
 
+href = DomProp "href" :: DomProp String
+
 disabled = DomProp "disabled" :: DomProp Boolean
+
+checked = DomProp "checked" :: DomProp Boolean
